cb488d9857cc5539d4d3233578272623654265e6
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-04-05 21:09:27 +0900

    [Gold IV] Title: 트리의 지름, Time: 76 ms, Memory: 36916 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+# 트리 구현
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n-1):
+    a,b,c = map(int,input().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+
+
+def dfs(x, wei):
+    for i in graph[x]:
+        a,b =i
+        if distance[a] == -1:
+            distance[a] = wei + b
+            dfs(a, wei + b)
+
+# dfs로 1번 노드에서 가장 먼 곳 찾기
+distance = [-1]*(n+1)
+distance[1] = 0
+dfs(1,0)
+
+start = distance.index(max(distance))
+distance = [-1]*(n+1)
+distance[start] = 0
+dfs(start,0)
+
+print(max(distance))

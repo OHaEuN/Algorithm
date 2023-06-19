0962094e871696d00cb6bd2d0df19cf674f99d79
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-06-19 15:17:00 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 n,k = map(int,input().split())
 dic = dict()
 for _ in range(n):
@@ -8,14 +5,10 @@ for _ in range(n):
     dic[a] = [b,c,d]
 sorteddic = sorted(dic.items(), reverse=True, key = lambda x: (x[1][0],x[1][1],x[1][2]))
 rank = dict()
-order = 1
-for i in range(len(dic)):
-    if i == 0 :
-        rank[sorteddic[i][0]] = order
-    elif i > 0 and sorteddic[i][1] == sorteddic[i-1][1]:
-        rank[sorteddic[i][0]] = order
-    else:
-        order+=1
-        rank[sorteddic[i][0]] = order
+for i in range(len(sorteddic)):
+    rank[sorteddic[i][0]] = sorteddic[i][1]
 
-print(rank[k])
+for i in range(0,k):
+    if rank[k] == rank[i+1]:
+        print(i+1)
+        break

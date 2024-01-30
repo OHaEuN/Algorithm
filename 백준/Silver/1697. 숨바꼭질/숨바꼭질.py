fcfd3c73cb9a8218b77073f544428ee522a90c13
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-01-30 16:39:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35288 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,20 @@
+import sys
+inputf = sys.stdin.readline
+from collections import deque
+
+def bfs(v):
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if v == k:
+            return array[v]
+        for next_v in (v-1, v+1, 2*v):
+            if 0 <= next_v < MAX and not array[next_v]:
+                array[next_v] = array[v] + 1
+                q.append(next_v)
+
+
+MAX = 100001
+n, k = map(int, inputf().split())
+array = [0] * MAX
+print(bfs(n))

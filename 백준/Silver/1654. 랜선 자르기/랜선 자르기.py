590dd56fe49f27c7bd6b3b2654c3c1a64e164d13
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-10-05 20:39:16 +0900

    [Silver II] Title: 랜선 자르기, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,23 +1,23 @@
 import sys
-
 inputf = sys.stdin.readline
 
-K,N = map(int,inputf().split())
-arr=[]
+K, N = map(int, inputf().split())
+arr = []
 for _ in range(K):
-    arr.append(int(input()))
+    arr.append(int(inputf()))
 
-start=1
-end=max(arr)
-
-while start<=end:
-    mid = (start+end)//2
+s = 1
+e = max(arr)
+ans = 0
+while s <= e:
+    mid = (s + e)//2
     cnt = 0
-    for n in arr:
-        cnt += n//mid
-    if cnt >= N:
-        start = mid+1
+    for a in arr:
+        cnt += a//mid
+    if cnt>=N:
+        ans = mid 
+        s = mid+1
     else:
-        end = mid-1
-        
-print(end)
+        e = mid-1 
+
+print(ans)

950bfe952d705a06d3b1d2cc66a5274b3b7f0690
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-10-05 21:18:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,33 @@
 import sys
 inputf = sys.stdin.readline
-from collections import deque
 
 N = int(inputf())
-ans = []
-mp = []
-for _ in range(N):
-    mp.append(list(map(int,inputf().strip())))
 visited = [[0 for _ in range(N)] for _ in range(N)]
-dx = [1,-1,0,0]
-dy = [0,0,-1,1]
+arr = [list(inputf().rstrip()) for _ in range(N)]
 
 def bfs(x,y):
-    cnt = 0
-    q = deque([(x,y)])
+    q = [(x,y)]
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
     visited[y][x] = 1
+    cnt = 0
     while q:
-        cnt += 1
-        X,Y = q.popleft()
+        x,y=q.pop(0)
+        cnt +=1
         for i in range(4):
-            mx = X+dx[i]
-            my = Y+dy[i]
-            if 0<=mx<N and 0<=my<N:
-                if not visited[my][mx]:
-                    visited[my][mx] = 1
-                    if mp[my][mx] == 1:
-                        q.append((mx,my))
-                        
-    ans.append(cnt)
+            nx, ny = x+dx[i], y+dy[i]
+            if 0<=nx<N and 0<=ny<N:
+                if not visited[ny][nx] and arr[ny][nx] == '1':
+                    q.append((nx,ny))
+                    visited[ny][nx] = 1
+    return cnt
 
-for x in range(N):
-    for y in range(N):
-        if not visited[y][x]:
-            if mp[y][x] == 1:
-                bfs(x,y)
-
-print(len(ans))
+ans = []
+for i in range(N):
+    for j in range(N):
+        if not visited[j][i] and arr[j][i] =='1':
+            ans.append(bfs(i,j))
 ans.sort()
+print(len(ans))
 for a in ans:
     print(a)

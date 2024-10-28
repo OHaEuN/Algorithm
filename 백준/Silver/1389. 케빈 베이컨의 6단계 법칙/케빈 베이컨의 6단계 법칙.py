17d3049ff65b6fac5e511e22888aabb4cf7e57ae
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-10-28 20:35:42 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,29 +1,32 @@
 import sys
+
 inputf = sys.stdin.readline
-N,M = map(int,inputf().split()) 
+
+N,M = map(int,inputf().split())
 connect = [[] for _ in range(N+1)]
-answer = [0 for _ in range(N)]
+
 for _ in range(M):
     a,b = map(int,inputf().split())
     connect[a].append(b)
     connect[b].append(a)
-    
-def count(x):
-    global answer
-    kevin = [0]*(N+1)
-    visited=[0 for _ in range(N+1)]
-    queue = []
-    queue.append(x)
-    visited[x] = 1
-    while len(queue)>0:
-        n = queue.pop(0)
-        for i in connect[n]:
-            if visited[i]==0:
-                queue.append(i)
-                visited[i]=1
-                kevin[i]=kevin[n]+1
-    answer[x-1]=sum(kevin)
-        
+
+def visit(i):
+    visited = [0 for _ in range(N+1)]
+    q = [i]
+    visited[i] = 0
+    while q:
+        p = q.pop(0)
+        for f in connect[p]:
+            if not visited[f]:
+                visited[f] = visited[p]+1
+                q.append(f)
+    return sum(visited)
+
+ans = 0
+k = float("inf")
 for i in range(1,N+1):
-    count(i)
-print(answer.index(min(answer))+1)
+    n = visit(i)
+    if n < k:
+        ans = i
+        k = n
+print(ans)

f788e3e90d6e370ea8ddb626f1b1a8cbe4c82cb5
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-11-10 19:38:16 +0900

    [Gold V] Title: 토마토, Time: 1816 ms, Memory: 57552 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -2,35 +2,33 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-
 M, N, H = map(int,input().split())
 
 matrix =[[list(map(int,input().split())) for _ in range(N)] for _ in range(H)]
 visited = [[[False]*M for _ in range(N)] for _ in range(H)]
 
-
 dx= [-1,1,0,0,0,0]
 dy= [0,0,-1,1,0,0]
 dz= [0,0,0,0,-1,1]
 
-queue = deque()
+q = deque()
 cnt = 0
 
 def bfs():
-    while queue:
-        x,y,z = queue.popleft()
+    while q:
+        x,y,z = q.popleft()
 
-        for i in range(6): # 주변 토마토 탐색
+        for i in range(6):
             nx = x + dx[i]
             ny = y + dy[i]
             nz = z + dz[i]
 
-            if nx < 0 or nx >= H or ny < 0 or ny >= N or nz < 0 or nz >= M: # 범위 값 벗어나면
+            if nx < 0 or nx >= H or ny < 0 or ny >= N or nz < 0 or nz >= M: 
                 continue
 
-            if matrix[nx][ny][nz] == 0 and visited[nx][ny][nz] == False: # 안익은 토마토면서 방문 x인 경우
+            if matrix[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
                 matrix[nx][ny][nz] = matrix[x][y][z] + 1 
-                queue.append((nx,ny,nz)) # 새로 익은 토마토 주변 탐색해야 하니까 queue에 append
+                q.append((nx,ny,nz))
                 visited[nx][ny][nz] = True
 
 # 익은 토마토 좌표 queue에 넣은 다음 bfs 실행
@@ -38,11 +36,10 @@ for a in range(H):
     for b in range(N):
         for c in range(M):
             if matrix[a][b][c] == 1 and visited[a][b][c] == False:
-                queue.append((a,b,c))
+                q.append((a,b,c))
                 visited[a][b][c] = True
 bfs()
 
-# 토마토 확인
 for box in matrix:
     for row in box:
         for num in row:
@@ -52,4 +49,3 @@ for box in matrix:
         cnt = max(cnt, max(row))
 
 print(cnt - 1)
-# 모두 1인 경우 0 출력되므로 예외 처리 필요 없음

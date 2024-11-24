1252ab6af8a795c5c1a343bd03473be71db91681
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2024-11-24 21:05:50 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 39604 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,17 @@
+import sys
+inputf = sys.stdin.readline
+
+N= int(inputf())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int,inputf().split())))
+max_arr =[[0] * i  for i in range(1,N+1)]
+max_arr[0][0] = arr[0][0]
+for i in range(N-1):
+    max_arr[i+1][0] = max_arr[i][0]+arr[i+1][0]
+    max_arr[i+1][-1] = max_arr[i][-1]+arr[i+1][-1]
+for i in range(2,N):
+    for j in range(1,i):
+        max_arr[i][j] = max(max_arr[i-1][j-1],max_arr[i-1][j]) + arr[i][j]
+
+print(max(max_arr[N-1]))
